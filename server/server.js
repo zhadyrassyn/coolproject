630af86c07df80cb6da49ffb44a38af89263e7b8
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-09 20:18:33 +0600

    lesson2

--- server/server.js
+++ server/server.js
@@ -20,16 +20,42 @@ const myPost = new Post({
   author: 'Amirzhan'
 });
 
-myPost.save()
-  .then(function(success) {
-    console.log('success ', success);
-  })
-  .catch(function(error) {
-    console.log('error ', error);
-  });
+// myPost.save()
+//   .then(function(success) {
+//     console.log('success ', success);
+//   })
+//   .catch(function(error) {
+//     console.log('error ', error);
+//   });
 
 app.get('/', function(req, res) {
   res.send('Hello, world, Decoce');
+});
+
+/* GET ALL POSTS */
+app.get('/api/posts', function(req, res) {
+  Post.find().then(function(posts) {
+    res.send({
+      posts
+    });
+  }).catch(function(error) {
+    console.log('error ', error);
+    res.sendStatus(400);
+  });
+});
+
+//ID: 5bb77bcd2919b41e80b0f4eb
+app.get('/api/posts/:id', function(req, res) {
+  var idParam = req.params.id;
+  Post.findById(idParam).then(function(post) {
+    console.log('success ', post);
+    res.send({
+      post: post
+    })
+  }).catch(function(error) {
+    console.log('error ', error);
+    res.send(error);
+  })
 });
 
 var port = 3000;
