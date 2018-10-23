cd046181641c8acac93461d268a4d81e3453ddb9
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-23 19:16:00 +0600

    add modal

--- client/public/feed/feed-component.js
+++ client/public/feed/feed-component.js
@@ -1,8 +1,24 @@
 angular
   .module('feedModule')
   .component('feedComponent', {
-    controller: function() {
+    controllerAs: 'feedController',
+    controller: function($scope, $http) {
 
+      $scope.school = "Decode";
+
+      $scope.showAddModal = false;
+
+      $scope.posts = [];
+      $http.get('/api/posts')
+        .then(function(success) {
+          $scope.posts = success.data.posts;
+        }).catch(function(error) {
+          console.log('error ', error);
+        });
+
+      $scope.showAddModalEvent = function() {
+        $scope.showAddModal = true;
+      }
     },
     templateUrl: '/feed/feed.html'
   });
