e959761942540260dd2a5a73267824c08d6b2420
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-30 20:33:48 +0600

    post detail

--- client/public/angular.module.js
+++ client/public/angular.module.js
@@ -1,1 +1,22 @@
-var app = angular.module('app', ['feedModule', 'postDetailModule']);
+var app = angular.module('app', ['ui.router', 'feedModule', 'postDetailModule']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  var states = [
+    {
+      name: 'index',
+      url: '/',
+      component: 'feedComponent'
+    },
+    {
+      name: 'postDetail',
+      url: '/detail/{postID}',
+      component: 'postDetailComponent'
+    }
+  ];
+
+  $urlRouterProvider.otherwise('/');
+
+  states.forEach(state => $stateProvider.state(state));
+});
--- client/public/post-detail/post-detail.component.js
+++ client/public/post-detail/post-detail.component.js
@@ -1,8 +1,21 @@
 angular
   .module('postDetailModule')
   .component('postDetailComponent', {
-    controller: function($scope) {
+    controller: function($scope, $http, $state) {
       $scope.school = "Decode";
+      $scope.post = {};
+
+      var id = $state.params.postID;
+      var url = '/api/posts/' + id;
+      $http.get(url)
+        .then(function(response) {
+          if (response.status === 200) {
+            $scope.post = response.data.post;
+          }
+        })
+        .catch(function(error) {
+          console.log('error ', error);
+        })
     },
     templateUrl: '/post-detail/post-detail.html'
   });
